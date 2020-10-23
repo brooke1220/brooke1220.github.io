8b216281ea97716b1164cd22eccd301223580730
brooke <overbob@yeah.net>
2020-10-23 22:54:50 +0800

    1

--- verify.js
+++ verify.js
@@ -1,2 +1,15 @@
 document.title = '请立即停止您的行为';
-document.body.appendChild('<div style="font-size: 16px;position: fixed;top: 10px;left: 10px;z-index: 99999999;color: #fff;background: #000;width: 200px;line-height: 100px;text-align: center;">请立即停止您的行为</div>');
+
+var divElement = document.createElement('div');
+document.body.appendChild(divElement);
+divElement.style.fontSize = '16px';
+divElement.style.position = 'fixed';
+divElement.style.top = '10px';
+divElement.style.left = '10px';
+divElement.style.zIndex = '99999999';
+divElement.style.color = '#fff';
+divElement.style.background = '#000';
+divElement.style.width = '200px';
+divElement.style.lineHeight = '100px';
+divElement.style.textAlign = 'center';
+divElement.innerHTML = '请立即停止您的行为';
